4114fdec483ca1fee6af665f407a15c4d016bae0
Phenixel <yonathancardoso@outlook.fr>
2024-07-02 11:22:52 +0200

    problèmes régler ça marche

--- app.py
+++ app.py
@@ -1,11 +1,37 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+import os
+import dotenv
+
+# Use dotenv to load environment variables from .env file
+dotenv.load_dotenv()
+
+# Get the DATABASE_URL environment variable
+database_url = os.getenv('DATABASE_URL')
 
 app = Flask(__name__)
 
+# Configure SQLAlchemy with the database URL
+app.config['SQLALCHEMY_DATABASE_URI'] = database_url
+db = SQLAlchemy(app)
+
+
+class Person(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(120), nullable=False)
+
+
+# Create all database tables
+with app.app_context():
+    db.create_all()
+
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def hello_world():
+    # Retrieve the list of "Person" from the mariadb database
+    persons = Person.query.all()
+    return render_template('index.html', persons=persons)
 
 
 if __name__ == '__main__':
